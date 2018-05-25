a3f50945eb68ed8f55649d7bb9a93bfee0bf68af
Sam <samosbor@byu.edu>
2018-05-25 13:01:49 -0600

    5/25

---
+++ src/main/java/Server.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.net.*;
+import com.sun.net.httpserver.*;
+
+import handler.RegisterHandler;
+
+public class Server {
+    private static final int MAX_WAITING_CONNECTIONS = 12;
+    private HttpServer server;
+
+    private void run(String portNumber) {
+        System.out.println("Initializing HTTP Server");
+
+        try {
+            // Create a new HttpServer object.
+            // Rather than calling "new" directly, we instead create
+            // the object by calling the HttpServer.create static factory method.
+            // Just like "new", this method returns a reference to the new object.
+            server = HttpServer.create(new InetSocketAddress(Integer.parseInt(portNumber)), MAX_WAITING_CONNECTIONS);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        server.setExecutor(null);
+
+
+        server.createContext("/user/register", new RegisterHandler());
+    }
+
+}
---
+++ src/main/java/handler/RegisterHandler.java
@@ -0,0 +1,11 @@
+package handler;
+
+import com.sun.net.httpserver.HttpHandler;
+
+public class RegisterHandler implements HttpHandler {
+
+    public RegisterHandler() {
+    }
+
+
+}
